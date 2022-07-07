32b522ef1a1f05768e2b25b881fe718879299e63
czerospace <czerospace@163.com>
2022-07-07 23:00:17 +0800

    add mysql

---
+++ lesson27-Handle_MySQL_Base/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@tcp(192.168.137.132:3306)/gin_demo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	// 尝试建立连接
+	if err := db.Ping(); err != nil {
+		fmt.Println("connect to database failed...")
+		panic(err)
+	}
+
+	fmt.Println("connect to database success")
+}
